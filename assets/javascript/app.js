5e42be7892bb459a9f4fd6789d17ebdd33bf47c9
Peter Geyser <peterpgeyser@gmail.com>
2019-07-24 17:00:06 -0700

    Question bank complete

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -14,36 +14,101 @@ $(document).ready(function () {
         $('#page-end').hide();
         $('#page-intro').hide();
 
-        //var questionBank = [
-            //{
-                //"question": "Ipsum lorem?",
-                //"correct_answer": "ipsum",
-                //"incorrect_answers": [
-                    //"imsum",
-                    //"lorem",
-                    //"istrum"
-                //]
-            //},
-            //{
-                //"question": "Ipsum lorem?",
-                //"correct_answer": "ipsum",
-                //"incorrect_answers": [
-                    //"imsum",
-                    //"lorem",
-                    //"istrum"
-                //]
-            //},
-            //{
-                //"question": "Ipsum lorem?",
-                //"correct_answer": "ipsum",
-                //"incorrect_answers": [
-                    //"imsum",
-                    //"lorem",
-                    //"istrum"
-                //]
-            //}
-        //];
+        var questionBank = [
+            {
+                "question": "What is Erin's real first name?",
+                "correct_answer": "Kelly",
+                "incorrect_answers": [
+                    "Holly",
+                    "Jan",
+                    "Megan"
+                ]
+            },
+            {
+                "question": "How many times do Jan and Michael have sex while in Jamaica?",
+                "correct_answer": "Six Times",
+                "incorrect_answers": [
+                    "One Time",
+                    "Twelve Times",
+                    "Two Times"
+                ]
+            },
+            {
+                "question": "Where do Jim and Pam have their honeymoon?",
+                "correct_answer": "Puerto Rico",
+                "incorrect_answers": [
+                    "Costa Rica",
+                    "Jamaica",
+                    "Hawaii"
+                ]
+            },
+            {
+                "question": "what is the subject of the painting stuck to wall in Jim's parents' house?",
+                "correct_answer": "A Clown",
+                "incorrect_answers": [
+                    "Jim and his brothers as children",
+                    "A bowl of fruit",
+                    "The ocean"
+                ]
+            },
+            {
+                "question": "What car does Dwight buy from Andy and then immediately re-sell?",
+                "correct_answer": "Nissan X-Terra",
+                "incorrect_answers": [
+                    "Toyota Prius",
+                    "Subaru Impreza",
+                    "Cadillac Escalade"
+                ]
+            },
+            {
+                "question": "Which one of Angela's cats does Dwight stick in the freezer?",
+                "correct_answer": "Sprinkles",
+                "incorrect_answers": [
+                    "Princess Lady",
+                    "Bandit",
+                    "Mr. Ash"
+                ]
+            },
+            {
+                "question": "What is the name of the nightclub Michael and Dwight meet Ryan at in New York City?",
+                "correct_answer": "Prerogative",
+                "incorrect_answers": [
+                    "Serenity",
+                    "The Amazon",
+                    "The Powder Room"
+                ]
+            },
+            {
+                "question": "What movie do Michael and Holly spoof in their company picnic skit?",
+                "correct_answer": "Slumdog Millionaire",
+                "incorrect_answers": [
+                    "Die Hard",
+                    "Jaws",
+                    "Back to the Future"
+                ]
+            },
+            {
+                "question": "What is Katy selling at the office before going on a date with Jim?",
+                "correct_answer": "Purses",
+                "incorrect_answers": [
+                    "Catalogs",
+                    "Insurance",
+                    "Pretzels"
+                ]
+            },
+            {
+                "question": "What is Roy's last name?",
+                "correct_answer": "Anderson",
+                "incorrect_answers": [
+                    "Smith",
+                    "Wallace",
+                    "Vance"
+                ]
+            },
+        ];
 
+        console.log(questionBank);
+    
         //var correct = 0
         //var incorrect = 0
         //var unanswered = 0
